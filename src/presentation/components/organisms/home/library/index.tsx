87e3bacefbc4b3b547e11f99c388c2927fc2c8c7
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-11-19 11:15:39 -0500

    feat: create section library in home

---
+++ src/presentation/components/organisms/home/library/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Library = () => {
+	return (
+		<>
+			<div>
+				<div className="relative px-[104px] pt-[96px] pb-[56px] h-[720px]">
+					<div className="absolute top-0 left-0 w-full h-full -z-10">
+						<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/library-home.webp" className="object-cover" alt="events" fill />
+					</div>
+					<div className="absolute top-0 left-0 w-full h-full -z-10 bg-[rgba(23,26,29,0.64)]"></div>
+					<div className="container h-full flex items-center justify-center">
+						<div className="max-w-[606px] text-center">
+							<h2 className="text-[32px] font-bold leading-[normal]">Biblioteca España de las Artes</h2>
+							<p className="mt-2 leading-[24px]">Contamos con una gran variedad de recursos bibliográficos en diferentes formatos y disciplinas; siendo también un espacio de recreación y fomento de actividades culturales.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Library
