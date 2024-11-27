f951069f92b2c094eb3dda41f8f051c3b4a789f2
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-11-27 16:38:35 -0500

    feat: add button "contactanos" in home

--- src/infrastructure/ui/components/organisms/home/contact/index.tsx
+++ src/infrastructure/ui/components/organisms/home/contact/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Contact: React.FC = () => {
@@ -9,7 +10,9 @@ const Contact: React.FC = () => {
 						<h2 className="text-[32px] font-semibold leading-[normal] max-w-[337px]">Reserva una visita guiada</h2>
 						<p className="font-medium leading-[24px] max-w-[523px]">Vive una experiencia única en el corazón de la universidad más antigua de América ¡Te esperamos!</p>
 					</div>
-					<p>Contáctanos</p>
+					<Link className="p-4 border-[1px] border-white rounded-lg flex items-center" href="/contactanos">
+						<span className="leading-[16px] font-semibold inline-block">Contáctanos</span>
+					</Link>
 				</div>
 			</div>
 		</>
