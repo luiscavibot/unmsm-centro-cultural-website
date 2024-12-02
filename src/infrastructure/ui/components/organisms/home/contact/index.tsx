c38738ec773f3166ff979cec17235db46e2e7cbc
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-12-02 10:17:23 -0500

    feat: update bg in contact section

--- src/infrastructure/ui/components/organisms/home/contact/index.tsx
+++ src/infrastructure/ui/components/organisms/home/contact/index.tsx
@@ -5,7 +5,7 @@ import OutlineButton from '@/infrastructure/ui/components/atoms/buttons/outline-
 const Contact: React.FC = () => {
 	return (
 		<>
-			<div className="px-[104px] gap-5 bg-green text-white py-[45px] bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-contactanos.svg')] bg-no-repeat bg-[-47px}_-100px]">
+			<div className="px-[104px] gap-5 bg-green text-white py-[45px] bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-contactanos.svg')] bg-no-repeat bg-[-47px_-100px]">
 				<div className="container flex flex-row items-center justify-between">
 					<div className="flex flex-row items-center justify-center">
 						<h2 className="text-[32px] font-semibold leading-[normal] max-w-[337px]">Reserva una visita guiada</h2>
