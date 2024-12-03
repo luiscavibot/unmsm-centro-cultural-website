3c0b7f4398e8e02647d51510a5c87bc47927878f
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-12-03 10:45:48 -0500

    feat: update bg in museums section

--- src/infrastructure/ui/components/organisms/home/museums/index.tsx
+++ src/infrastructure/ui/components/organisms/home/museums/index.tsx
@@ -17,7 +17,7 @@ const Museums = () => {
 			bgRef1.current.style.transform = 'scale(1.05)';
 		}
 		if (coverRef1.current) {
-			coverRef1.current.style.transform = 'scale(2.6)';
+			coverRef1.current.style.transform = 'scale(3.16)';
 		}
 	};
 
@@ -35,7 +35,7 @@ const Museums = () => {
 			bgRef2.current.style.transform = 'scale(1.05)';
 		}
 		if (coverRef2.current) {
-			coverRef2.current.style.transform = 'scale(2.6)';
+			coverRef2.current.style.transform = 'scale(3.16)';
 		}
 	};
 
