686d5ee05e07a119101e965f958aa9dbe50c8f11
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-12-03 10:43:14 -0500

    feat: add Link in exhibitions section

--- src/infrastructure/ui/components/organisms/home/exhibitions/index.tsx
+++ src/infrastructure/ui/components/organisms/home/exhibitions/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Exhibitions = () => {
@@ -6,66 +7,71 @@ const Exhibitions = () => {
 		<section id="exposiciones" className="scroll-mt-28">
 			<h2 className="hidden sr-only">Exposiciones</h2>
 			<div className="grid grid-cols-2 grid-rows-[repeat(3,360px)]">
-				<article className="row-span-2 pl-[104px] h-full relative group">
-					<figure className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
-						<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/tesoros-arqueologicos.webp" className="object-cover transform origin-[left_center] group-hover:scale-[1.204] transition-transform duration-[800ms] ease-out" alt="Tesoros Arqueológicos de San Marcos" fill />
-					</figure>
-					{/* <div className="absolute top-0 left-0 w-full h-full -z-10 bg-[rgba(28,6,7,0.64)]"></div> */}
-					<div className="relative pt-[96px] pb-[56px] pr-16 semi-container-l h-full flex items-end">
-						<header className="max-w-[512px]">
-							<h3 className="text-[32px] font-bold leading-[39px]">Tesoros Arqueológicos de San Marcos</h3>
-							<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
-						</header>
-					</div>
-				</article>
-				<article className="pr-[104px] h-full relative group">
-					<figure className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
-						<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/casa-del-saber.webp" className="object-cover transform origin-[right_top] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="La universidad como «casa del saber»: Religiosos y laicos" fill />
-					</figure>
-					{/* <div className="absolute top-0 left-0 w-full h-full -z-10 bg-[rgba(28,6,7,0.64)]"></div> */}
-					<div className="relative pt-[96px] pb-[56px] pl-16 semi-container-r h-full flex items-end">
-						<header className="max-w-[512px]">
-							<h3 className="text-[32px] font-bold leading-[39px]">La universidad como «casa del saber»: Religiosos y laicos</h3>
-							<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
-						</header>
-					</div>
-				</article>
-				<article className="pr-[104px] h-full relative group">
-					<div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
-						<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/arte-moderno.webp" className="object-cover transform origin-[right_center] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="Arte moderno y contemporáneo" fill />
-					</div>
-					{/* <div className="absolute top-0 left-0 w-full h-full -z-10 bg-[rgba(28,6,7,0.64)]"></div> */}
-					<div className="relative pt-[96px] pb-[56px] pl-16 semi-container-r h-full flex items-end">
-						<header className="max-w-[512px]">
-							<h3 className="text-[32px] font-bold leading-[39px]">Arte moderno y contemporáneo</h3>
-							<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
-						</header>
-					</div>
-				</article>
-				<article className="pl-[104px] h-full relative group">
-					<div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
-						<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/buen-vivir.webp" className="object-cover transform origin-[left_bottom] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="Buen Vivir / Sumaq Kawsay / Suma Qamaña / Jakonax jatikaxbi jati" fill />
-					</div>
-					{/* <div className="absolute top-0 left-0 w-full h-full -z-10 bg-[rgba(28,6,7,0.64)]"></div> */}
-					<div className="relative pt-[96px] pb-[56px] pr-16 semi-container-l h-full flex items-end">
-						<header className="max-w-[512px]">
-							<h3 className="text-[32px] font-bold leading-[39px]">Buen Vivir / Sumaq Kawsay / Suma Qamaña / Jakonax jatikaxbi jati</h3>
-							<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
-						</header>
-					</div>
-				</article>
-				<article className="pr-[104px] h-full relative group">
-					<div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
-						<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/mario-vargas-llosa.webp" className="object-cover transform origin-[right_bottom] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="Mario Vargas Llosa" fill />
-					</div>
-					{/* <div className="absolute top-0 left-0 w-full h-full -z-10 bg-[rgba(28,6,7,0.64)]"></div> */}
-					<div className="relative pt-[96px] pb-[56px] pl-16 semi-container-r h-full flex items-end">
-						<header className="max-w-[512px]">
-							<h3 className="text-[32px] font-bold leading-[39px]">Mario Vargas Llosa</h3>
-							<p className="mt-2 font-medium leading-[24px]">Sala</p>
-						</header>
-					</div>
-				</article>
+				<Link className="row-span-2 group" href="#">
+					<article className="relative w-full h-full pl-[104px]">
+						<figure className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
+							<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/tesoros-arqueologicos.webp" className="object-cover transform origin-[left_center] group-hover:scale-[1.204] transition-transform duration-[800ms] ease-out" alt="Tesoros Arqueológicos de San Marcos" fill />
+						</figure>
+						<div className="relative pt-[96px] pb-[56px] pr-16 semi-container-l h-full flex items-end">
+							<header className="max-w-[512px]">
+								<h3 className="text-[32px] font-bold leading-[39px]">Tesoros Arqueológicos de San Marcos</h3>
+								<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
+							</header>
+						</div>
+					</article>
+				</Link>
+				<Link href="#" className="group">
+					<article className="pr-[104px] h-full relative">
+						<figure className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
+							<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/casa-del-saber.webp" className="object-cover transform origin-[right_top] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="La universidad como «casa del saber»: Religiosos y laicos" fill />
+						</figure>
+						<div className="relative pt-[96px] pb-[56px] pl-16 semi-container-r h-full flex items-end">
+							<header className="max-w-[512px]">
+								<h3 className="text-[32px] font-bold leading-[39px]">La universidad como «casa del saber»: Religiosos y laicos</h3>
+								<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
+							</header>
+						</div>
+					</article>
+				</Link>
+				<Link href="#" className="group">
+					<article className="pr-[104px] h-full relative">
+						<div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
+							<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/arte-moderno.webp" className="object-cover transform origin-[right_center] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="Arte moderno y contemporáneo" fill />
+						</div>
+						<div className="relative pt-[96px] pb-[56px] pl-16 semi-container-r h-full flex items-end">
+							<header className="max-w-[512px]">
+								<h3 className="text-[32px] font-bold leading-[39px]">Arte moderno y contemporáneo</h3>
+								<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
+							</header>
+						</div>
+					</article>
+				</Link>
+				<Link href="#" className="group">
+					<article className="pl-[104px] h-full relative">
+						<div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
+							<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/buen-vivir.webp" className="object-cover transform origin-[left_bottom] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="Buen Vivir / Sumaq Kawsay / Suma Qamaña / Jakonax jatikaxbi jati" fill />
+						</div>
+						<div className="relative pt-[96px] pb-[56px] pr-16 semi-container-l h-full flex items-end">
+							<header className="max-w-[512px]">
+								<h3 className="text-[32px] font-bold leading-[39px]">Buen Vivir / Sumaq Kawsay / Suma Qamaña / Jakonax jatikaxbi jati</h3>
+								<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
+							</header>
+						</div>
+					</article>
+				</Link>
+				<Link href="#" className="group">
+					<article className="pr-[104px] h-full relative">
+						<div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
+							<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/mario-vargas-llosa.webp" className="object-cover transform origin-[right_bottom] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="Mario Vargas Llosa" fill />
+						</div>
+						<div className="relative pt-[96px] pb-[56px] pl-16 semi-container-r h-full flex items-end">
+							<header className="max-w-[512px]">
+								<h3 className="text-[32px] font-bold leading-[39px]">Mario Vargas Llosa</h3>
+								<p className="mt-2 font-medium leading-[24px]">Sala</p>
+							</header>
+						</div>
+					</article>
+				</Link>
 			</div>
 		</section>
 	)
