a4e031167b4d5c9d23a815b6e53079601521d6ef
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-12-03 10:55:17 -0500

    feat: update menu

--- src/infrastructure/ui/components/organisms/shared/header/navigation-menu/navigation-menu.tsx
+++ src/infrastructure/ui/components/organisms/shared/header/navigation-menu/navigation-menu.tsx
@@ -55,7 +55,7 @@ const FlyoutLink: FC<FlyoutLinkProps> = ({ children, href, external, FlyoutConte
 					<>
 						{
 							external ? (
-								<a href={href} rel="noopener noreferrer" target="_blank" className="text-sm relative h-[49px] inline-flex items-center leading-[16.8px] p-4 text-white">
+								<a href={href} rel="noopener noreferrer" target="_blank" className="text-sm font-semibold relative h-[49px] inline-flex items-center leading-[16.8px] p-4 text-white">
 									{children}
 									<span
 										style={{
@@ -67,7 +67,7 @@ const FlyoutLink: FC<FlyoutLinkProps> = ({ children, href, external, FlyoutConte
 							)
 								:
 								(
-									<Link href={href} className="text-sm relative h-[49px] inline-flex items-center leading-[16.8px] p-4 text-white">
+									<Link href={href} className="text-sm font-semibold relative h-[49px] inline-flex items-center leading-[16.8px] p-4 text-white">
 										{children}
 										<span
 											style={{
@@ -82,7 +82,7 @@ const FlyoutLink: FC<FlyoutLinkProps> = ({ children, href, external, FlyoutConte
 				)
 					:
 					(
-						<div className="text-sm relative h-[49px] inline-flex items-center leading-[16.8px] p-4 text-white cursor-default">
+						<div className="text-sm font-semibold relative h-[49px] inline-flex items-center leading-[16.8px] p-4 text-white cursor-default">
 							{children}
 							<span
 								style={{
@@ -101,7 +101,7 @@ const FlyoutLink: FC<FlyoutLinkProps> = ({ children, href, external, FlyoutConte
 						exit={{ opacity: 0, y: 15 }}
 						style={{ translateX: "-50%" }}
 						transition={{ duration: 0.3, ease: "easeOut" }}
-						className="absolute left-1/2 top-[51px] bg-dark-gray"
+						className="absolute left-1/2 top-[51px] bg-transparent"
 					>
 						<div className="absolute -top-[2px] left-0 right-0 h-[2px] bg-transparent" />
 						{/* <div className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white" /> */}
@@ -115,23 +115,23 @@ const FlyoutLink: FC<FlyoutLinkProps> = ({ children, href, external, FlyoutConte
 
 const ArtisticDirectionsContent: FC = () => {
 	return (
-		<div className="w-[204px] text-white py-2 shadow-xl">
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+		<div className="w-[204px] text-white py-2 shadow-xl font-medium rounded-lg overflow-hidden bg-dark-gray">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Ballet
 			</Link>
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Folklore
 			</Link>
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Cine
 			</Link>
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Música
 			</Link>
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Teatro
 			</Link>
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Banda universitaria
 			</Link>
 		</div>
@@ -140,14 +140,14 @@ const ArtisticDirectionsContent: FC = () => {
 
 const MuseumsContent: FC = () => {
 	return (
-		<div className="w-[204px] text-white py-2 shadow-xl">
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+		<div className="w-[204px] text-white py-2 shadow-xl font-medium rounded-lg overflow-hidden bg-dark-gray">
+			<Link href="#" className="flex items-center px-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Museo de Arte de San Marcos
 			</Link>
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+			<Link href="#" className="flex items-center px-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Museo de Arqueología y Antropología
 			</Link>
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Exposiciones
 			</Link>
 		</div>
@@ -156,14 +156,14 @@ const MuseumsContent: FC = () => {
 
 const VisitUsContent: FC = () => {
 	return (
-		<div className="w-[204px] text-white py-2 shadow-xl">
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+		<div className="w-[204px] text-white py-2 shadow-xl font-medium rounded-lg overflow-hidden bg-dark-gray">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Visitas guiadas
 			</Link>
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Dirección de turismo
 			</Link>
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Casona de San Marcos
 			</Link>
 		</div>
@@ -172,11 +172,11 @@ const VisitUsContent: FC = () => {
 
 const AboutUsContent: FC = () => {
 	return (
-		<div className="w-[204px] text-white py-2 shadow-xl">
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+		<div className="w-[204px] text-white py-2 shadow-xl font-medium rounded-lg overflow-hidden bg-dark-gray">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Transparencia
 			</Link>
-			<Link href="#" className="flex imtes-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
+			<Link href="#" className="flex items-center p-4 h-[48px] leading-[16.8px] text-sm hover:bg-gray transition-colors duration-300">
 				Directorio
 			</Link>
 		</div>
