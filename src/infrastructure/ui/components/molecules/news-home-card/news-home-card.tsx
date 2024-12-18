73628eb1a2dacc580a66441fd5a76a61ecb27cdb
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-12-17 20:00:50 -0500

    feat: update hover in news cards

--- src/infrastructure/ui/components/molecules/news-home-card/news-home-card.tsx
+++ src/infrastructure/ui/components/molecules/news-home-card/news-home-card.tsx
@@ -12,7 +12,7 @@ interface NewsHomeCardProps {
 }
 
 const NewsHomeCard: React.FC<NewsHomeCardProps> = ({ imageUrl, date, dateString, title, summary, slug }) => (
-	<Link href={`noticias/${slug}`}>
+	<Link className="group" href={`noticias/${slug}`}>
 		<article className="rounded-2xl overflow-hidden flex flex-row w-[620px] h-[240px]">
 			<figure className="relative w-[240px] h-[240px] flex-shrink-0">
 				<Image src={imageUrl} className="object-cover" alt={title} layout="fill" />
@@ -23,7 +23,7 @@ const NewsHomeCard: React.FC<NewsHomeCardProps> = ({ imageUrl, date, dateString,
 						<CalendarIcon className="shrink-0" ariaLabel="Fecha" color="dark" />
 						<time dateTime={date} className="text-dark-blue-2 text-sm leading-[21px]">{dateString}</time>
 					</div>
-					<h3 className="line-clamp-3 text-xl text-dark-blue font-semibold leading-[30px]">{title}</h3>
+					<h3 className="line-clamp-3 text-xl text-dark-blue font-semibold leading-[30px] group-hover:text-dark-red transition-colors duration-200">{title}</h3>
 				</header>
 				<p className="line-clamp-3 text-dark-blue-2 text-sm leading-[21px] mt-1">{summary}</p>
 			</div>
