cc9fe8e3258549c8c64bdc180f54eb31d18fbfee
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-12-20 01:19:31 -0500

    feat: update responsive in exhibitions

--- src/infrastructure/ui/components/organisms/home/exhibitions/index.tsx
+++ src/infrastructure/ui/components/organisms/home/exhibitions/index.tsx
@@ -12,7 +12,7 @@ const Exhibitions = () => {
 						<figure className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
 							<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/tesoros-arqueologicos.webp" className="object-cover transform origin-[left_center] group-hover:scale-[1.204] transition-transform duration-[800ms] ease-out" alt="Tesoros Arqueológicos de San Marcos" fill />
 						</figure>
-						<div className="relative pt-[56px] lg:pt-[96px] pb-[56px] lg:pr-16 semi-container-l h-full flex items-end">
+						<div className="relative pt-[56px] lg:pt-[96px] pb-[56px] lg:pr-16 lg:semi-container-l h-full flex items-end">
 							<header className="max-w-[512px] text-white">
 								<h3 className="text-[24px] lg:text-[32px] font-bold leading-[normal] lg:leading-[39px]">Tesoros Arqueológicos de San Marcos</h3>
 								<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
@@ -25,7 +25,7 @@ const Exhibitions = () => {
 						<figure className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
 							<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/casa-del-saber.webp" className="object-cover transform origin-[right_top] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="La universidad como «casa del saber»: Religiosos y laicos" fill />
 						</figure>
-						<div className="relative pt-[56px] lg:pt-[96px] pb-[56px] lg:pl-16 semi-container-r h-full flex items-end">
+						<div className="relative pt-[56px] lg:pt-[96px] pb-[56px] lg:pl-16 lg:semi-container-r h-full flex items-end">
 							<header className="max-w-[512px] text-white">
 								<h3 className="text-[24px] lg:text-[32px] font-bold leading-[normal] lg:leading-[39px]">La universidad como «casa del saber»: Religiosos y laicos</h3>
 								<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
@@ -38,7 +38,7 @@ const Exhibitions = () => {
 						<figure className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
 							<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/arte-moderno.webp" className="object-cover transform origin-[right_center] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="Arte moderno y contemporáneo" fill />
 						</figure>
-						<div className="relative pt-[56px] lg:pt-[96px] pb-[56px] lg:pl-16 semi-container-r h-full flex items-end">
+						<div className="relative pt-[56px] lg:pt-[96px] pb-[56px] lg:pl-16 lg:semi-container-r h-full flex items-end">
 							<header className="max-w-[512px] text-white">
 								<h3 className="text-[24px] lg:text-[32px] font-bold leading-[normal] lg:leading-[39px]">Arte moderno y contemporáneo</h3>
 								<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
@@ -51,7 +51,7 @@ const Exhibitions = () => {
 						<figure className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
 							<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/buen-vivir.webp" className="object-cover transform origin-[left_bottom] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="Buen Vivir / Sumaq Kawsay / Suma Qamaña / Jakonax jatikaxbi jati" fill />
 						</figure>
-						<div className="relative pt-[56px] lg:pt-[96px] pb-[56px] lg:pr-16 semi-container-l h-full flex items-end">
+						<div className="relative pt-[56px] lg:pt-[96px] pb-[56px] lg:pr-16 lg:semi-container-l h-full flex items-end">
 							<header className="max-w-[512px] text-white">
 								<h3 className="text-[24px] lg:text-[32px] font-bold leading-[normal] lg:leading-[39px]">Buen Vivir / Sumaq Kawsay / Suma Qamaña / Jakonax jatikaxbi jati</h3>
 								<p className="mt-2 font-medium leading-[24px]">Exposición permanente</p>
@@ -64,7 +64,7 @@ const Exhibitions = () => {
 						<figure className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
 							<Image src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/mario-vargas-llosa.webp" className="object-cover transform origin-[right_bottom] group-hover:scale-[1.194] transition-transform duration-[800ms] ease-out" alt="Mario Vargas Llosa" fill />
 						</figure>
-						<div className="relative pt-[56px] lg:pt-[96px] pb-[56px] lg:pl-16 semi-container-r h-full flex items-end">
+						<div className="relative pt-[56px] lg:pt-[96px] pb-[56px] lg:pl-16 lg:semi-container-r h-full flex items-end">
 							<header className="max-w-[512px] text-white">
 								<h3 className="text-[24px] lg:text-[32px] font-bold leading-[normal] lg:leading-[39px]">Mario Vargas Llosa</h3>
 								<p className="mt-2 font-medium leading-[24px]">Sala</p>
