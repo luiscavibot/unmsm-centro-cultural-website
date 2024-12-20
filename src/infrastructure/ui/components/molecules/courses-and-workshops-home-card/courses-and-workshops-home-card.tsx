b08df57f79b9e8d170a2c4f09a3e276e3ca29137
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-12-19 19:27:52 -0500

    feat: add responsive in "talleres" in home

--- src/infrastructure/ui/components/molecules/courses-and-workshops-home-card/courses-and-workshops-home-card.tsx
+++ src/infrastructure/ui/components/molecules/courses-and-workshops-home-card/courses-and-workshops-home-card.tsx
@@ -38,7 +38,7 @@ const CoursesAndWorkshopsHomeCard: FC<CoursesAndWorkshopsHomeCardProps> = ({
 	};
 
 	return (
-		<article className="relative px-[104px] pt-[96px] pb-[56px] h-[720px]">
+		<article className="relative px-6 lg:px-[104px] pt-[64px] lg:pt-[96px] pb-[56px] h-[420px] lg:h-[720px]">
 			<figure className="absolute top-0 left-0 w-full h-full -z-10">
 				<Image
 					src={imageUrl}
@@ -57,14 +57,14 @@ const CoursesAndWorkshopsHomeCard: FC<CoursesAndWorkshopsHomeCardProps> = ({
 					<div className="max-w-[459px]">
 						<header>
 							<Badge label={type} size="large" />
-							<h3 className="text-[40px] font-bold leading-[normal] text-white">
+							<h3 className="text-[24px] lg:text-[40px] font-bold leading-[normal] text-white">
 								{title}
 							</h3>
 							<p className="mt-2 text-xl font-semibold leading-[20px] text-white">
 								{subtitle}
 							</p>
 						</header>
-						<p className="mt-6 font-medium leading-[24px] text-white">
+						<p className="max-lg:hidden mt-6 font-medium leading-[24px] text-white line-clamp-[10]">
 							{description}
 						</p>
 					</div>
