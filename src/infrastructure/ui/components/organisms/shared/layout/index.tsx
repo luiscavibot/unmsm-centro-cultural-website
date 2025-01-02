f6e5d3d6da9c7ed8c8f21f453ae1df14a8df109e
Hanhusaso <hans.e.huiza.n@gmail.com>
2025-01-02 14:01:36 -0500

    feat: update height in layout

--- src/infrastructure/ui/components/organisms/shared/layout/index.tsx
+++ src/infrastructure/ui/components/organisms/shared/layout/index.tsx
@@ -16,7 +16,7 @@ interface LayoutProps {
 const Layout = ({ children, breadcrumbItems, portadaImage }: LayoutProps) => {
 	return (
 		<section className="relative h-full">
-			<div id="imagen" className="fixed top-[--header-top-height] left-0 w-full h-[544px] z-[-1]">
+			<div id="imagen" className="fixed top-[--total-header-height] left-0 w-full h-[544px] z-[-1]">
 				<Image
 					// src="https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-agenda-cultural.webp"
 					src={portadaImage}
@@ -25,7 +25,7 @@ const Layout = ({ children, breadcrumbItems, portadaImage }: LayoutProps) => {
 					objectFit="cover"
 				/>
 			</div>
-			<div id="contenido" className="pt-[369px]">
+			<div id="contenido" className="pt-[422px]">
 				<div className="px-4 lg:px-[104px] bg-white pt-16 rounded-t-[32px] overflow-hidden">
 					<div className="container">
 						<Breadcrumb items={breadcrumbItems} />
