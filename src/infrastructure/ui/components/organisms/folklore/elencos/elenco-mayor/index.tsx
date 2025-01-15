37a946c339f0226da43e7cfcf20f65bc5cbac156
Hanhusaso <hans.e.huiza.n@gmail.com>
2025-01-15 10:48:26 -0500

    feat: add cine

--- src/infrastructure/ui/components/organisms/folklore/elencos/elenco-mayor/index.tsx
+++ src/infrastructure/ui/components/organisms/folklore/elencos/elenco-mayor/index.tsx
@@ -34,7 +34,7 @@ const ElencoMayor: React.FC<Props> = ({
 		<div className="px-4 lg:px-[104px] gap-5 bg-black text-white py-8 lg:py-[96px] bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-1.svg')] bg-no-repeat bg-[-123px_10px]">
 			<div className="container flex flex-col gap-y-8 lg:grid lg:grid-cols-[auto_minmax(0,1fr)] items-start justify-between gap-x-8 relative">
 				<div className="max-w-[394px] mt-0 lg:mt-10">
-					<h3 className="text-[32px] leading-[normal] font-semibold">Arte moderno y contemporáneo</h3>
+					<h3 className="text-[32px] leading-[normal] font-semibold">Elenco mayor</h3>
 					<p className="font-medium leading-[24px] mt-6">
 						El Elenco Mayor de Danzas es la unidad artística representativa del Centro Universitario de Folklore en materia de danza. Sus inicios data de 1970, desde la conformación misma del CUF y sus integrantes son escogidos en su totalidad de las diferentes facultades de la UNMSM. Este elenco representa oficialmente a la Decana de América en diversos eventos a los que se le invita, siempre con gran calidad interpretativa y gran reconocimiento del público. Se torna además en un colectivo de gran integración profesional y cultural, donde se busca también complementar la formación profesional e integral de sus miembros con las virtudes y valores de la danza folklórica peruana.
 					</p>
