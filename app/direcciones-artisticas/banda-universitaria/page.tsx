7e8bb3144a1af67e83aadaec26022c1bd22ed631
Hanhusaso <hans.e.huiza.n@gmail.com>
2025-03-17 05:35:16 -0500

    fix: add location, hours, contact information, and social media links to Banda Universitaria page

--- app/direcciones-artisticas/banda-universitaria/page.tsx
+++ app/direcciones-artisticas/banda-universitaria/page.tsx
@@ -1,4 +1,9 @@
 import SecondaryButton from '@/infrastructure/ui/components/atoms/buttons/secondary-button';
+import ClockIcon from '@/infrastructure/ui/components/atoms/icons/clock-icon';
+import OutlinePlaceIcon from '@/infrastructure/ui/components/atoms/icons/outilne-place-icon';
+import OutlineEmailIcon from '@/infrastructure/ui/components/atoms/icons/outline-email-icon';
+import FacebookIcon from '@/infrastructure/ui/components/atoms/icons/social/facebook-icon';
+import InstagramIcon from '@/infrastructure/ui/components/atoms/icons/social/instagram-icon';
 import Title from '@/infrastructure/ui/components/atoms/title';
 import Layout from '@/infrastructure/ui/components/organisms/shared/layout';
 import Link from 'next/link';
@@ -46,6 +51,89 @@ export default function BandaUniversitaria() {
 							>
 								Conoce la historia
 							</Link>
+							<div className="max-w-[639px] mx-auto mt-14">
+								<div className="leading-[24px] space-y-1 text-dark-blue-2">
+									<div className="flex p-1 gap-2 items-start">
+										<OutlinePlaceIcon
+											className="shrink-0 mt-[2px]"
+											ariaLabel="Lugar"
+											color="dark"
+										/>
+										<div>
+											<p className="font-bold">Lugar</p>
+											<p className="font-medium">
+												Centro cultural UNMSM
+												<br />
+												Av. Nicolás de Piérola 1222.
+												Parque Universitario. Centro
+												Histórico de Lima
+											</p>
+										</div>
+									</div>
+									<div className="flex p-1 gap-2 items-start">
+										<ClockIcon
+											className="shrink-0 mt-[2px]"
+											ariaLabel="Horario"
+											color="dark"
+										/>
+										<div>
+											<p className="font-bold">Horario de atención en oficina</p>
+											<p className="font-medium">
+												Lunes a viernes de 9:30 a.m. a 1 p.m. y de 2 p.m. a 5 p.m.
+											</p>
+										</div>
+									</div>
+									<div className="flex p-1 gap-2 items-start">
+										<OutlineEmailIcon
+											className="shrink-0 mt-[2px]"
+											ariaLabel="Contacto"
+											color="dark"
+										/>
+										<div>
+											<p className="font-bold">
+												Contacto
+											</p>
+											<p className="font-medium">
+												bandauniversitaria.ccsm@unmsm.edu.pe
+											</p>
+										</div>
+									</div>
+								</div>
+								<div className="flex flex-row items-center gap-2 mt-5 ml-7">
+									<a
+										className="p-1 rounded-[4px] bg-dark-gray-2"
+										href="https://www.instagram.com/bandauniversitaria_unmsm/"
+										rel="noreferrer noopener"
+										target="_blank"
+									>
+										<InstagramIcon />
+									</a>
+									<a
+										className="p-1 rounded-[4px] bg-dark-gray-2"
+										href="https://www.facebook.com/BandaUniversitariaSanMarcos/?locale=es_LA"
+										rel="noreferrer noopener"
+										target="_blank"
+									>
+										<FacebookIcon />
+									</a>
+									{/* <a
+										className="p-1 rounded-[4px] bg-dark-gray-2"
+										href="https://www.tiktok.com/@museoarte.unmsm"
+										rel="noreferrer noopener"
+										target="_blank"
+									>
+										<TiktokIcon />
+									</a> */}
+									{/* <a
+										className="p-1 rounded-[4px] bg-dark-gray-2"
+										href=""
+										rel="noreferrer noopener"
+										target="_blank"
+									>
+										<YoutubeIcon />
+									</a> */}
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
