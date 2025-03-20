6a838e358494213b9255159ac9cf899ca659a5f9
Hanhusaso <hans.e.huiza.n@gmail.com>
2025-03-20 02:21:13 -0500

    fix: enhance text clarity and conciseness in MusicaHistoria component

--- app/direcciones-artisticas/musica/historia/page.tsx
+++ app/direcciones-artisticas/musica/historia/page.tsx
@@ -36,40 +36,13 @@ export default function MusicaHistoria() {
 							<Title className="text-center max-w-[437px] mx-auto">
 								Historia de la dirección de Música
 							</Title>
-							<div className="leading-[24px] text-dark-blue-2 mb-4 space-y-10">
+							<div className="leading-[24px] text-dark-blue-2 mb-4">
 								<div>
 									<p className="mb-5">
-										En los primeros días del mes de
-										setiembre de 1954 un grupo de alumnos
-										sanmarquinos, dirigidos por Rosa Alarco,
-										se reúnen en el salón de Grados de la
-										Facultad de Letras. La actividad que
-										iban a realizar no era considerada en el
-										contenido académico de la vida
-										universitaria, pues la música y en
-										especial el canto coral eran totalmente
-										extraños a San Marcos. Pero algunos
-										estudiantes, entre los que destacaban
-										los directivos de la Federación
-										Universitaria de San Marcos opinaban que
-										la música y la actividad coral ayudaría
-										a redescubrir muestra identidad
-										nacional. Entre estos dirigentes
-										destacaba Alfonso Barrantes, quien apoyó
-										de manera decisiva esta iniciativa. Así,
-										gracias a esta suma de voluntades se fue
-										configurando la personalidad artística y
-										la vida del coro universitario de San
-										Marcos. *
+										En los primeros días del mes de setiembre de 1954 un grupo de alumnos sanmarquinos, dirigidos por Rosa Alarco, se reúnen en el salón de Grados de la Facultad de Letras. La actividad que iban a realizar no era considerada en el contenido académico de la vida universitaria, pues la música y en especial el canto coral eran totalmente extraños a San Marcos. Pero algunos estudiantes, entre los que destacaban los directivos de la Federación Universitaria de San Marcos opinaban que la música y la actividad coral ayudaría a redescubrir muestra identidad nacional. Entre estos dirigentes destacaba Alfonso Barrantes, quien apoyó de manera decisiva esta iniciativa. Así, gracias a esta suma de voluntades se fue configurando la personalidad artística y la vida del coro universitario de San Marcos.
 									</p>
 									<p className="mb-5">
-										El Coro Universitario de San Marcos
-										CUSM, fue fundado en 1961, a propuesta
-										de la Federación de Estudiantes. Su
-										directora fundadora fue la Sra. Rosa
-										Alarco Larrabure quien estuvo al frente
-										del Coro con mucho éxito hasta el año
-										1976. *
+										El Coro Universitario de San Marcos CUSM, fue fundado en 1961, a propuesta de la Federación de Estudiantes. Su directora fundadora fue la Sra. Rosa Alarco Larrabure quien estuvo al frente del Coro con mucho éxito hasta el año 1976. 
 									</p>
 								</div>
 								{/* <div>
@@ -85,89 +58,34 @@ export default function MusicaHistoria() {
 							</div> */}
 								<div>
 									<p className="mb-5">
-										En su gran trayectoria artística ha
-										ofrecido más de un millar de conciertos
-										didácticos y populares, en Pueblos
-										Jóvenes, Comunidades Campesinas, Centros
-										de Reeducación, fabricas, Universidades.
-										Colegios, etc. Ha recorrido el Perú en
-										todas sus regiones, habiendo trascendido
-										las fronteras nacionales. A partir de
-										1971 ha venido organizando anualmente en
-										ocasión de su aniversario (23 de
-										noviembre) las NOCHES CORALES
-										UNIVERSITARIAS, con participación de
-										todos los grupos corales de Centros
-										Educativos Superiores de Lima. Debemos
-										agregar que el CUSM estuvo integrado por
-										estudiantes, trabajadores y por personas
-										aficionadas al arte coral*, como hasta
-										la fecha.
+										En su gran trayectoria artística ha ofrecido más de un millar de conciertos didácticos y populares, en Pueblos Jóvenes, Comunidades Campesinas, Centros de Reeducación, fabricas, Universidades, Colegios, etc. Ha recorrido el Perú en todas sus regiones, habiendo trascendido las fronteras nacionales. A partir de 1971 ha venido organizando anualmente en ocasión de su aniversario (23 de noviembre) las NOCHES CORALES UNIVERSITARIAS, con participación de todos los grupos corales de Centros Educativos Superiores de Lima. Debemos agregar que el CUSM estuvo integrado por estudiantes, trabajadores y por personas aficionadas al arte coral, como hasta la fecha.
 									</p>
 									<p className="mb-5">
-										Según el Dr. Guillermo Thomberry. el
-										Coro de San Marcos trabajó algunos meses
-										en 1941 bajo la dirección del músico
-										Alejandro Koseff Integró el elenco con
-										alumnos de varias facultades. * El
-										diario La Prensa que con fecha 5 de
-										setiembre de 1954, informaba que el
-										número de adherentes crecía de reunión
-										en reunión. Por lo que deducimos que la
-										prensa y la intelectualidad limeña
-										siempre estaban atentos a lo que ocurría
-										en San Marcos, más aún cuando una
-										artista de trayectoria ya reconocida
-										como era Rosa Alarco, aparecía como
-										responsable de dicho evento. * El primer
-										concierto formal se realiza el día
-										jueves 20 del mes de diciembre de 1955
-										en el Salón de Grados de la Universidad
-										(Capilla Nuestra Señora de Loreto). *
+										Según el Dr. Guillermo Thomberry,  el Coro de San Marcos trabajó algunos meses en 1941 bajo la dirección del músico Alejandro Koseff Integró el elenco con alumnos de varias facultades. 
+										El diario La Prensa que con fecha 5 de setiembre de 1954, informaba que el número de adherentes crecía de reunión en reunión. Por lo que deducimos que la prensa y la intelectualidad limeña siempre estaban atentos a lo que ocurría en San Marcos, más aún cuando una artista de trayectoria ya reconocida como era Rosa Alarco, aparecía como responsable de dicho evento. 
+										El primer concierto formal se realiza el día jueves 20 del mes de diciembre de 1955 en el Salón de Grados de la Universidad (Capilla Nuestra Señora de Loreto). 
 									</p>
 									<p className="mb-5">
-										Las autoridades universitarias argüían
-										no contar con el presupuesto necesario
-										para formar el coro de la Universidad.
-										Esta circunstancia puso a prueba la
-										voluntad de Rosa Alarco, quien sin
-										amilanarse perseveró en su propósito y
-										se quedó hasta 1958 sin recibir
-										emolumento alguno. La posibilidad de
-										trabajar con un coro mixto a capella y
-										el contagiante entusiasmo de los alumnos
-										*
+										Las autoridades universitarias argüían no contar con el presupuesto necesario para formar el coro de la Universidad. Esta circunstancia puso a prueba la voluntad de Rosa Alarco, quien sin amilanarse perseveró en su propósito y se quedó hasta 1958 sin recibir emolumento alguno. La posibilidad de trabajar con un coro mixto a capella y el contagiante entusiasmo de los alumnos.
 									</p>
 									<p className="mb-1">
-										El Coro Universitario de San Marcos CUSM
-										ha tenido entres sus directores: Rosa
-										Alarco Larrabure* musicóloga (Perú 1911
-										-1980) “…Su trabajo siguió siendo
-										ad-honorem hasta que las autoridades de
-										la universidad aprueban, mediante la
-										Resolución 18989 del 30 de setiembre de
-										1961, su contratación para dirigir el
-										Coro Universitario de San Marcos. * lo
-										cual lo hizo hasta 1976. Forman parte de
-										este contingente de músicos dedicados a
-										la dirección coral, los directores que
-										se sucedieron en el Coro Sanmarquino.
-										Ellos son en orden temporal según su
-										gestión:
+										El Coro Universitario de San Marcos CUSM ha tenido entres sus directores:
+										Rosa Alarco Larrabure musicóloga (Perú 1911 -1980) “...Su trabajo siguió siendo ad-honorem hasta que las autoridades de la universidad aprueban, mediante la Resolución 18989 del 30 de setiembre de 1961, su contratación para dirigir el Coro Universitario de San Marcos. * lo cual lo hizo hasta 1976.
+										Forman parte de este contingente de músicos dedicados a la dirección coral, los directores que se sucedieron en el Coro Sanmarquino. Ellos son en orden temporal según su gestión:
 									</p>
 									<ul className="list-none">
-										<li>Abel Rosas (1975-1979) *</li>
-										<li>Nancy Pimentel (1980) *</li>
-										<li>Luis Craff (1981¬1992) *</li>
-										<li>Carlos Bendezú (1992 - 1996) *</li>
-										<li>Nancy Pimentel (1997) *</li>
-										<li>Rene Peñaloza (1998-2000) *</li>
+										<li>Abel Rosas (1975-1979)</li>
+										<li>Nancy Pimentel (1980)</li>
+										<li>Luis Craff (1981-1992)</li>
+										<li>Carlos Bendezú (1992 - 1996)</li>
+										<li>Nancy Pimentel (1997)</li>
+										<li>Rene Peñaloza (1998-2000)</li>
 										<li>
 											Israel Wladimir Olaya Pérez
 											(2000-2007)
 										</li>
 										<li>
-											Jhon Michael Gómez García (2007-)
+											Jhon Michael Gómez García (2007)
 										</li>
 										<li>
 											En el año 2009 la Dirección de Coro
@@ -183,7 +101,7 @@ export default function MusicaHistoria() {
 										</li>
 										<li>
 											Enrique Vargas Machuca Acevedo
-											(2023-mzo 2024)
+											(2023-marzo 2024)
 										</li>
 									</ul>
 								</div>
@@ -200,11 +118,7 @@ export default function MusicaHistoria() {
 							</div> */}
 								<div>
 									<p className="mb-5">
-										*Fuente: San Marcos y la música popular:
-										historia del coro universitario
-										1954-2001/ César Coronel Moscoso/
-										Universidad Nacional Mayor de San
-										Marcos.
+										En la actualidad el Coro Universitario está bajo la dirección de los Maestros José Mendieta Echevarría y Rafael González Carranza, quienes tienen a su cargo los cursos de Técnica vocal y lectura musical de la Dirección de Música del Centro Cultural de San Marcos.
 									</p>
 								</div>
 							</div>
