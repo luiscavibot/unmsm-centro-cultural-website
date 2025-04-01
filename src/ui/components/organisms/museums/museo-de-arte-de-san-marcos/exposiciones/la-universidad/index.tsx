79f6f85ecdb2a01125a473abb5e5efae81e40d50
Hanhusaso <hans.e.huiza.n@gmail.com>
2025-04-01 16:39:26 -0500

    fix: enhance layout responsiveness for multiple components by adjusting max-width and flex properties

--- src/ui/components/organisms/museums/museo-de-arte-de-san-marcos/exposiciones/la-universidad/index.tsx
+++ src/ui/components/organisms/museums/museo-de-arte-de-san-marcos/exposiciones/la-universidad/index.tsx
@@ -35,7 +35,7 @@ const LaUniversidad: React.FC<Props> = ({
 }) => {
 	return (
 		<div className="px-4 lg:px-[104px] gap-5 bg-dark-red-3 text-white py-8 lg:py-[96px] bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-3.svg')] bg-no-repeat bg-[-123px_10px]">
-			<div className="container flex flex-col gap-y-8 lg:grid lg:grid-cols-[minmax(0,1fr)_auto] items-start justify-between gap-x-8 relative">
+			<div className="container flex flex-col-reverse lg:flex-col gap-y-8 lg:grid lg:grid-cols-[minmax(0,1fr)_auto] items-start justify-between gap-x-8 relative">
 				<div className="max-lg:w-full">
 					<Splide
 						onMoved={handleMoveExpoUniversidad}
@@ -47,7 +47,7 @@ const LaUniversidad: React.FC<Props> = ({
 							{laUniversidadComocasaDelSaberReligiososYLaicosData.map(
 								(img, index) => (
 									<SplideSlide key={index}>
-										<figure className="relative w-auto h-full flex-shrink-0 rounded-2xl overflow-hidden">
+										<figure className="relative w-auto h-full flex-shrink-0 lg:rounded-2xl overflow-hidden">
 											<Image
 												src={img.url}
 												className="max-lg:object-contain w-auto h-full mx-auto"
@@ -62,7 +62,7 @@ const LaUniversidad: React.FC<Props> = ({
 							)}
 						</SplideTrack>
 					</Splide>
-					<div className="flex justify-start gap-x-2 relative mt-4 pointer-events-none">
+					<div className="flex justify-end lg:justify-start gap-x-2 relative mt-4 pointer-events-none">
 						<ArrowButton
 							className="pointer-events-auto"
 							theme="light"
@@ -79,7 +79,7 @@ const LaUniversidad: React.FC<Props> = ({
 						/>
 					</div>
 				</div>
-				<div className="max-w-[394px]">
+				<div className="lg:max-w-[394px]">
 					<span className="text-sm font-semibold mb-1 inline-block">
 						Exposición permanente
 					</span>
