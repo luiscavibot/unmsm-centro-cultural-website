148b6159f25ad10927b55fdd87a5e5b186dacb33
Hanhusaso <hans.e.huiza.n@gmail.com>
2025-04-01 17:10:32 -0500

    fix: enhance layout responsiveness for Publicaciones component by adjusting flex properties and padding

--- src/ui/components/organisms/museums/museo-de-arqueologia-y-antropologia/publicaciones/index.tsx
+++ src/ui/components/organisms/museums/museo-de-arqueologia-y-antropologia/publicaciones/index.tsx
@@ -19,18 +19,18 @@ const Publicaciones = () => {
 			</div>
 			<div className="px-4 lg:px-[104px] bg-dark-white-2 pb-20">
 				<div className="container">
-					<div className="flex gap-6 mb-6">
+					<div className="flex flex-col lg:flex-row gap-6 mb-6">
 						<a
 							href="https://cedoc.sisbib.unmsm.edu.pe/biblioteca_digital/revistas/cuadernos-de-investigacion-del-archivo-tello?pagina=1"
 							rel="noopener noreferrer"
 							target="_blank"
-							className="basis-1/2 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue-2.svg')] bg-no-repeat bg-[-160px_105px] text-white rounded-lg min-h-[240px] px-12 py-14 flex justify-between gap-2"
+							className="basis-1/2 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue-2.svg')] bg-no-repeat bg-[-160px_105px] text-white rounded-lg lg:min-h-[240px] px-6 lg:px-12 py-10 lg:py-14 flex justify-between gap-2"
 						>
 							<div>
 								<span className="block mb-1 font-semibold leading-[24px]">
 									Revista
 								</span>
-								<span className="block text-[32px] leading-[normal] font-bold">
+								<span className="block text-2xl lg:text-[32px] leading-[normal] font-bold">
 									Cuadernos de Investigación del Archivo Tello
 									<br />
 									(N° 1 al N° 15)
@@ -44,13 +44,13 @@ const Publicaciones = () => {
 							href="https://fondoeditorial.unmsm.edu.pe/catalogo"
 							rel="noopener noreferrer"
 							target="_blank"
-							className="basis-1/2 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue.svg')] bg-no-repeat bg-[-153px_85px] text-white rounded-lg min-h-[240px] px-12 py-14 flex justify-between gap-2"
+							className="basis-1/2 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue.svg')] bg-no-repeat bg-[-153px_85px] text-white rounded-lg lg:min-h-[240px] px-6 lg:px-12 py-10 lg:py-14 flex justify-between gap-2"
 						>
 							<div>
 								<span className="block mb-1 font-semibold leading-[24px]">
 									Revista
 								</span>
-								<span className="block text-[32px] leading-[normal] font-bold">
+								<span className="block text-2xl lg:text-[32px] leading-[normal] font-bold">
 									Cuadernos de Investigación del Archivo Tello
 									<br />
 									(N° 16 y N° 17)
@@ -61,18 +61,18 @@ const Publicaciones = () => {
 							</div>
 						</a>
 					</div>
-					<div className="flex gap-6 mb-6">
+					<div className="flex flex-col lg:flex-row gap-6 mb-6">
 						<a
 							href="https://revistasinvestigacion.unmsm.edu.pe/index.php/Arqueo/issue/archive "
 							rel="noopener noreferrer"
 							target="_blank"
-							className="basis-1/3 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue-2.svg')] bg-no-repeat bg-[-160px_105px] text-white rounded-lg min-h-[240px] px-12 py-14 flex justify-between gap-2"
+							className="basis-1/3 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue-2.svg')] bg-no-repeat bg-[-160px_105px] text-white rounded-lg lg:min-h-[240px] px-6 lg:px-12 py-10 lg:py-14 flex justify-between gap-2"
 						>
 							<div>
 								<span className="block mb-1 font-semibold leading-[24px]">
 									Revista
 								</span>
-								<span className="block text-[32px] leading-[normal] font-bold">
+								<span className="block text-2xl lg:text-[32px] leading-[normal] font-bold">
 									Arqueología y Sociedad
 								</span>
 							</div>
@@ -84,13 +84,13 @@ const Publicaciones = () => {
 							href="https://sisbib.unmsm.edu.pe/bibvirtual/publicaciones/antropologia/default.htm"
 							rel="noopener noreferrer"
 							target="_blank"
-							className="basis-1/3 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue.svg')] bg-no-repeat bg-[-336px_85px] text-white rounded-lg min-h-[240px] px-12 py-14 flex justify-between gap-2"
+							className="basis-1/3 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue.svg')] bg-no-repeat bg-[-336px_85px] text-white rounded-lg lg:min-h-[240px] px-6 lg:px-12 py-10 lg:py-14 flex justify-between gap-2"
 						>
 							<div>
 								<span className="block mb-1 font-semibold leading-[24px]">
 									Boletín
 								</span>
-								<span className="block text-[32px] leading-[normal] font-bold">
+								<span className="block text-2xl lg:text-[32px] leading-[normal] font-bold">
 									MAA (1998-2005)
 								</span>
 							</div>
@@ -102,13 +102,13 @@ const Publicaciones = () => {
 							href="https://drive.google.com/drive/folders/1o9YzwwFsFpYGD6XhuB_YswE6vA7dWq03"
 							rel="noopener noreferrer"
 							target="_blank"
-							className="basis-1/3 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue.svg')] bg-no-repeat bg-[-233px_70px] text-white rounded-lg min-h-[240px] px-12 py-14 flex justify-between gap-2"
+							className="basis-1/3 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue.svg')] bg-no-repeat bg-[-233px_70px] text-white rounded-lg lg:min-h-[240px] px-6 lg:px-12 py-10 lg:py-14 flex justify-between gap-2"
 						>
 							<div>
 								<span className="block mb-1 font-semibold leading-[24px]">
 									Boletín
 								</span>
-								<span className="block text-[32px] leading-[normal] font-bold">
+								<span className="block text-2xl lg:text-[32px] leading-[normal] font-bold">
 									MAA (2021-2022)
 								</span>
 							</div>
@@ -117,18 +117,18 @@ const Publicaciones = () => {
 							</div>
 						</a>
 					</div>
-					<div className="flex gap-6">
+					<div className="flex flex-col lg:flex-row gap-6">
 						<a
 							href="https://drive.google.com/file/d/1CnHSJ-5Hzo5MUN3FTZMdpj3expw_TKNx/view?usp=drivesdk"
 							rel="noopener noreferrer"
 							target="_blank"
-							className="basis-1/2 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue-2.svg')] bg-no-repeat bg-[-160px_105px] text-white rounded-lg min-h-[240px] px-12 py-14 flex justify-between gap-2"
+							className="basis-1/2 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue-2.svg')] bg-no-repeat bg-[-160px_105px] text-white rounded-lg lg:min-h-[240px] px-6 lg:px-12 py-10 lg:py-14 flex justify-between gap-2"
 						>
 							<div>
 								<span className="block mb-1 font-semibold leading-[24px]">
 									Catálogo de exposiciones
 								</span>
-								<span className="block text-[32px] leading-[normal] font-bold">
+								<span className="block text-2xl lg:text-[32px] leading-[normal] font-bold">
 									Rebeca Carrión Cachot. La primera arqueóloga
 									peruana (2022)
 								</span>
@@ -141,13 +141,13 @@ const Publicaciones = () => {
 							href="https://drive.google.com/file/d/1YRqJKQq0RQlSGUaxsC9ahuoPgJ2825ja/view?usp=drivesdk"
 							rel="noopener noreferrer"
 							target="_blank"
-							className="basis-1/2 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue.svg')] bg-no-repeat bg-[-153px_85px] text-white rounded-lg min-h-[240px] px-12 py-14 flex justify-between gap-2"
+							className="basis-1/2 bg-blue-2 bg-[url('https://unmsm-static-files-v2.s3.us-east-2.amazonaws.com/centro-cultural-de-san-marcos/bg-waves-blue.svg')] bg-no-repeat bg-[-153px_85px] text-white rounded-lg lg:min-h-[240px] px-6 lg:px-12 py-10 lg:py-14 flex justify-between gap-2"
 						>
 							<div>
 								<span className="block mb-1 font-semibold leading-[24px]">
 									Catálogo de exposiciones
 								</span>
-								<span className="block text-[32px] leading-[normal] font-bold">
+								<span className="block text-2xl lg:text-[32px] leading-[normal] font-bold">
 									Toribio Mejía Xesspe. Brillante explorador
 									del Pasado andino (2023)
 								</span>
