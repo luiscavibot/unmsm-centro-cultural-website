ffa5a6756f22b88cd14ee95f30e4e9dd2bb52bfe
Hanhusaso <hans.e.huiza.n@gmail.com>
2025-04-07 11:36:50 -0500

    feat: implement CulturalAgendaPage with event listing, filtering, and pagination

--- app/agenda-cultural/page.tsx
+++ app/agenda-cultural/page.tsx
@@ -1,148 +1,28 @@
-'use client';
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
+import CulturalAgendaPage from './agenda-list-content';
+import { AgendaCulturalService } from '@/services/agenda-cultural.service';
 
-import Tab from '@/ui/components/atoms/tab';
-import Title from '@/ui/components/atoms/title';
-import EventsCard from '@/ui/components/molecules/events-card';
-import React, { useState } from 'react';
-import eventsDataToHome from '@/ui/mocks/events-data-to-home';
-import Calendar from '@/ui/components/molecules/calendar';
-import Pagination from '@/ui/components/molecules/pagination';
-import Layout from '@/ui/components/organisms/shared/layout';
-import AgendaFilter from '@/ui/components/organisms/agenda-cultural/agenda-filter';
-import PrimaryButton from '@/ui/components/atoms/buttons/primary-button';
-import FilterIcon from '@/ui/components/atoms/icons/filter-icon';
-import { AnimatePresence } from 'motion/react';
-import Modal from '@/ui/components/molecules/modal';
+export const revalidate = 60;
 
-type selectedTab = 'todos' | 'esta-semana';
+export default async function Page() {
 
-const PageSize = 2;
+const queryClient = new QueryClient();
+try {
+	// prefetch con validación adicional
+	// se replica en CulturalAgendaPage, pero al tener el mismo queryKey, no se vuelve a hacer fetch
+	await queryClient.prefetchQuery({
+		queryKey: ['list-agenda'],
+		queryFn: () => AgendaCulturalService.getEntriesToHome(),
+	});
+} catch (error) {
+	console.error("Error en prefetch:", error);
+	return <div>Error al cargar los datos de la agenda</div>;
+}
 
-const breadcrumbItems = [
-	{
-		title: 'Inicio',
-		path: '/',
-	},
-	{
-		title: 'Agenda cultural',
-		path: '/agenda-cultural',
-	},
-];
-
-export default function CulturalAgendaPage() {
-	const [selectedTab, setSelectedTab] = useState<selectedTab>('todos');
-
-	const [currentPage, setCurrentPage] = useState(1);
-
-	const [modalOpen, setModalOpen] = useState(false);
-
-	const close = () => setModalOpen(false);
-	const open = () => setModalOpen(true);
-
-	const handleClick = (tab: selectedTab) => {
-		setSelectedTab(tab);
-	};
-
-	return (
-		<Layout
-			portadaImage="https://ccsm.unmsm.edu.pe/ccsm/agenda_cultural_portada_c193f92394.jpg"
-			breadcrumbItems={breadcrumbItems}
-		>
-			<>
-				<div className="px-4 lg:px-[104px] bg-white pb-6">
-					<div className="container">
-						<div className="max-w-[814px] mx-auto mb-[80px]">
-							<Title className="text-center">
-								Agenda Cultural
-							</Title>
-							<div className="leading-[24px] text-dark-blue-2">
-								<p className="mb-3">
-									En este espacio te brindamos la agenda
-									mensual de las actividades que realizan las
-									diez direcciones que conforman el Centro
-									Cultural de San Marcos.
-								</p>
-								<p className="font-semibold">
-									El ingreso es libre, salvo las que se
-									indiquen.
-								</p>
-							</div>
-						</div>
-						<div className="flex gap-x-4">
-							<Tab
-								label="Todos"
-								selected={selectedTab === 'todos'}
-								onClick={() => {
-									handleClick('todos');
-								}}
-							/>
-							<Tab
-								label="Esta semana"
-								selected={selectedTab === 'esta-semana'}
-								onClick={() => {
-									handleClick('esta-semana');
-								}}
-							/>
-						</div>
-					</div>
-				</div>
-				<div className="px-4 lg:px-[104px] bg-dark-white-2 pt-[56px] pb-20 md:pb-[104px]">
-					<div className="container">
-						<div className="flex flex-col md:flex-row justify-between gap-x-8 xl:gap-x-[105px]">
-							<div>
-								<div className="mb-8 max-md:flex max-md:flex-row max-md:gap-x-4">
-									<Calendar className="grow" />
-									<div className="md:hidden">
-										<PrimaryButton
-											className="w-14 h-14"
-											icon={<FilterIcon />}
-											theme="light"
-											type="on-click"
-											onClick={() => (modalOpen ? close() : open())}
-										/>
-										<AnimatePresence
-											initial={false}
-											mode="wait"
-											onExitComplete={() => null}
-										>
-											{
-												modalOpen &&
-												<Modal handleClose={close} >
-													<AgendaFilter handleClose={close} />
-												</Modal>
-											}
-										</AnimatePresence>
-									</div>
-								</div>
-								<div className="max-md:hidden">
-									<AgendaFilter />
-								</div>
-							</div>
-							<div>
-								<span className="font-medium leading-[24px] text-left md:text-right flex items-end justify-start md:justify-end w-full mb-6 md:mb-8 md:h-[56px]">
-									57 resultados en total
-								</span>
-								<ul className="flex flex-col space-y-4 md:space-y-8">
-									{eventsDataToHome.map((event, index) => (
-										<li className="flex" key={index}>
-											<EventsCard {...event} />
-										</li>
-									))}
-								</ul>
-								<Pagination
-									className="pagination-bar"
-									currentPage={currentPage}
-									totalCount={eventsDataToHome.length}
-									pageSize={PageSize}
-									onPageChange={(page) =>
-										setCurrentPage(page)
-									}
-								/>
-							</div>
-						</div>
-					</div>
-				</div>
-			</>
-		</Layout>
-	);
+return (
+	<HydrationBoundary state={dehydrate(queryClient)}>
+		<CulturalAgendaPage />
+	</HydrationBoundary>
+);
 }
--- src/ui/components/molecules/events-card/events-card.tsx
+++ src/ui/components/molecules/events-card/events-card.tsx
@@ -4,53 +4,63 @@ import Badge from '../../atoms/badge';
 import DateRangeIcon from '../../atoms/icons/date-range-icon';
 import ClockIcon from '../../atoms/icons/clock-icon';
 import OutlinePlaceIcon from '../../atoms/icons/outilne-place-icon';
+import { AgendaCultural } from '@/interfaces/services/agenda-cultural.interface';
+import { getCustomDates } from '@/ui/helpers/get-custom-date';
 
-interface EventsCardProps {
-	slug: string;
-	url: string;
-	title: string;
-	summary: string;
-	date: string;
-	dateString: string;
-	time: string;
-	timeString: string;
-	location: string;
-	type: string;
-	dependency: string;
-}
+// interface EventsCardProps {
+// 	slug: string;
+// 	url: string;
+// 	title: string;
+// 	summary: string;
+// 	date: string;
+// 	dateString: string;
+// 	time: string;
+// 	timeString: string;
+// 	place: string;
+// 	type: string;
+// 	dependency: string;
+// 	image: string;
+// }
 
-const EventsCard: React.FC<EventsCardProps> = ({
+const EventsCard: React.FC<AgendaCultural> = ({
 	slug,
-	url,
+	image,
 	title,
 	summary,
-	date,
-	dateString,
-	time,
-	timeString,
-	location,
-	type,
-	dependency,
-}) => (
-	<Link className="inline-flex group" href={`agenda-cultural/${slug}`}>
-		<article className="rounded-2xl overflow-hidden flex flex-row w-auto group-focus:ring-2 group-active:ring-2 group-hover:ring-1 ring-dark-red transition-all duration-300">
+	place,
+	exact_dates,
+	date_ranges,
+	mode,
+	organizer,
+}) => {
+
+
+	const { daysSummary, singleDate } = getCustomDates(
+		exact_dates,
+		date_ranges
+	);
+
+	return (
+
+	<Link className="inline-flex group w-full" href={`agenda-cultural/${slug}`}>
+		<article className="rounded-2xl overflow-hidden flex flex-row w-auto group-focus:ring-2 group-active:ring-2 group-hover:ring-1 ring-dark-red transition-all duration-300 grow md:min-h-[346px]">
 			<figure className="relative w-[260px] h-full flex-shrink-0 max-md:hidden">
 				<Image
-					src={url}
+					src={image.url}
 					className="object-cover h-full"
 					alt={title}
 					fill
 				/>
 			</figure>
 			{/* <div className="bg-white p-6 w-[553px] flex-shrink-0"> */}
-			<div className="bg-white p-6 w-auto">
+			<div className="bg-white p-6 w-auto grow">
 				<header>
-					<Badge label={type} size="small" />
-					<h3 className="line-clamp-3 text-xl text-dark-blue font-semibold leading-[30px] group-hover:text-dark-red transition-colors duration-200">
+					<Badge label={mode} size="small" />
+					<h3 className="line-clamp-2 text-xl text-dark-blue font-semibold leading-[30px] group-hover:text-dark-red transition-colors duration-200">
 						{title}
 					</h3>
 					<span className="text-xs text-dark-blue-2">
-						Dependencia: {dependency}
+						Dependencia: {organizer}
 					</span>
 					<div className="leading-[normal] text-sm font-semibold mt-4 space-y-1">
 						<div className="flex p-1 gap-2 items-center">
@@ -59,27 +69,32 @@ const EventsCard: React.FC<EventsCardProps> = ({
 								ariaLabel="Fecha"
 								color="dark"
 							/>
-							<time className="block" dateTime={date}>
-								{dateString}
+							<time
+								className="block"
+								dateTime={daysSummary || singleDate?.day}
+							>
+								{daysSummary || singleDate?.day}
 							</time>
 						</div>
-						<div className="flex p-1 gap-2 items-center">
-							<ClockIcon
-								className="shrink-0"
-								ariaLabel="Hora"
-								color="dark"
-							/>
-							<time className="block" dateTime={time}>
-								{timeString}
-							</time>
-						</div>
+						{singleDate?.time && (
+							<div className="flex p-1 gap-2 items-center">
+								<ClockIcon
+									className="shrink-0"
+									ariaLabel="Hora"
+									color="dark"
+								/>
+								<time className="block">
+									{singleDate?.time}
+								</time>
+							</div>
+						)}
 						<div className="flex p-1 gap-2 items-center">
 							<OutlinePlaceIcon
 								className="shrink-0"
 								ariaLabel="Lugar"
 								color="dark"
 							/>
-							<p>{location}</p>
+							<p>{place}</p>
 						</div>
 					</div>
 				</header>
@@ -89,6 +104,7 @@ const EventsCard: React.FC<EventsCardProps> = ({
 			</div>
 		</article>
 	</Link>
-);
+	
+)};
 
 export default EventsCard;
