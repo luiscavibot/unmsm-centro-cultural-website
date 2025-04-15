4e01be65405a0dbe677439ce0a2a933bb0ca986a
Hanhusaso <hans.e.huiza.n@gmail.com>
2025-04-15 10:55:35 -0500

    style: mejorar estilos de clase para elementos de imagen, lista, encabezado y enlace

--- src/ui/components/molecules/block-renderer-client/block-renderer-client.tsx
+++ src/ui/components/molecules/block-renderer-client/block-renderer-client.tsx
@@ -20,7 +20,7 @@ export default function BlockRendererClient({
 				// console.log(image);
 				return (
 					<Image
-						className="mt-10 mb-10 w-full max-w-full"
+						className="mt-10 mb-10 mx-auto w-full max-w-full md:max-w-[90%] md:rounded-3xl"
 						src={image.url}
 						width={image.width}
 						height={image.height}
@@ -31,27 +31,27 @@ export default function BlockRendererClient({
 				},
 				list: ({ children, format }) => {
 					if (format === 'ordered') {
-						return <ol className="list-decimal list-outside ml-6">{children}</ol>;
+						return <ol className="list-decimal list-outside text-dark-blue-2 ml-6">{children}</ol>;
 					}
-					return <ul className="list-disc list-outside ml-6">{children}</ul>;
+					return <ul className="list-disc list-outside text-dark-blue-2 ml-6">{children}</ul>;
 				},
 				paragraph: ({ children }) => <p className="text-dark-blue-2 mb-5">{children}</p>,
 				heading: ({ children, level }) => {
 					const Tag = `h${level}` as keyof JSX.IntrinsicElements;
 					const sizes = {
-						1: 'text-3xl md:text-4xl font-bold mb-6',
-						2: 'text-2xl md:text-3xl font-semibold mb-6',
-						3: 'text-xl md:text-2xl font-semibold mb-6',
-						4: 'text-xl font-medium mb-6',
-						5: 'text-lg mb-6',
-						6: 'text-base mb-6',
+						1: 'text-3xl md:text-4xl font-bold text-dark-blue-2 mb-6',
+						2: 'text-2xl md:text-3xl font-semibold text-dark-blue-2 mb-6',
+						3: 'text-xl md:text-2xl font-semibold text-dark-blue-2 mb-6',
+						4: 'text-xl font-medium text-dark-blue-2 mb-6',
+						5: 'text-lg text-dark-blue-2 mb-6',
+						6: 'text-base text-dark-blue-2 mb-6',
 					};
 					return <Tag className={sizes[level] || ''}>{children}</Tag>;
 				},
 				link: ({ children, url }) => {
 					return (
 						<a
-							className="link"
+							className="link text-dark-blue-2"
 							href={url}
 							target="_blank"
 							rel="noopener noreferrer"
